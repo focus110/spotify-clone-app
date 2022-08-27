f9953a514fb60267c935734f6bb7040925523505
focus110 <iamemmaochuko@gmail.com>
2022-08-27 12:48:52 +0100

    Login ui, setup state context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+import Login from "./Login";
+import Player from "./Player";
+import { getTokenFromUrl } from "./spotify";
+import { useDataLayerValue } from "./DataLayer";
+
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
+
 function App() {
-  return <div className="text-2xl">Hello</div>;
+  const [token, setToken] = useState();
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  // run code based on th given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      dispatch({
+        type: "SET_USER",
+        token: _token,
+      });
+
+      //import spotify web api service
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
+    }
+
+    console.log("tk", _token);
+    // eslint-disable-next-line
+  }, []);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
